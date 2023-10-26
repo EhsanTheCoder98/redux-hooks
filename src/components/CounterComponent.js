32f3f33e0cae4fffc83e11c6dbb9fcbc5d65e29e
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2023-10-26 20:10:17 +0330

    first commit

---
+++ src/components/CounterComponent.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { increase } from '../redux/counter/counterAction';
+import { connect } from 'react-redux';
+
+const CounterComponent = (props) => {
+    return (
+        <div>
+            <h1>Number:{props.counter}</h1>
+            <button onClick={props.increase}>Increase</button>
+        </div>
+    );
+};
+
+const mapStateToProps = state => {
+    return{
+        counter:state.counter
+    }
+}
+const mapDispatchToProps = dispatch => {
+    return{
+        increase: ()=> dispatch(increase())
+    }
+}
+
+
+
+
+
+export default connect(mapDispatchToProps, mapStateToProps)(CounterComponent);
