ac418030aca7be90338150f672c85fb23e0650fe
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2023-10-27 15:26:45 +0330

    using logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
 import { legacy_createStore as createStore } from "redux";
+import { applyMiddleware } from "redux";
+import logger from "redux-logger";
 import rootReducers from "./rootReducer";
-const store = createStore(rootReducers);
+const store = createStore(rootReducers,applyMiddleware(logger));
 
 export default store;
