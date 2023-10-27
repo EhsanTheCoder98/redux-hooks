58cc10cea1d38b46f14d7cd9f6d566403d7cf6c0
Ehsan Fathalipour <www.ehsanfathalipour110@gmail.com>
2023-10-27 15:48:23 +0330

    using devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { legacy_createStore as createStore } from "redux";
 import { applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
 import rootReducers from "./rootReducer";
-const store = createStore(rootReducers,applyMiddleware(logger));
+const store = createStore(rootReducers,composeWithDevTools(applyMiddleware(logger)));
 
 export default store;
